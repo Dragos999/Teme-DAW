a776b28994f168a4442599f9d9fa913621db4ea9
Dragos999 <116753664+Dragos999@users.noreply.github.com>
2023-10-26 19:58:19 +0300

    Add files via upload

---
+++ Disciplina.cs
@@ -0,0 +1,10 @@
+﻿namespace tema3.Models
+{
+    public class Disciplina
+    {
+        public string Denumire { get; set; }
+        public int? NrOreSaptamanale { get; set; }
+        public string ProfesorPrincipal { get; set; }
+
+    }
+}
